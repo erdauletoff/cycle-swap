0a486adfcc3ba8e403b3557bc2ab5165e65c018e
Darkhan <darkhan_erdawletov@student.itpu.uz>
2023-10-24 00:24:47 +0500

    done!

--- src/main/java/com/epam/rd/autotasks/CycleSwap.java
+++ src/main/java/com/epam/rd/autotasks/CycleSwap.java
@@ -2,10 +2,27 @@ package com.epam.rd.autotasks;
 
 class CycleSwap {
     static void cycleSwap(int[] array) {
+        int len = array.length;
+        if (len > 0) {
+            int temp1 = array[0];
+            int temp2;
 
+            for (int i = 0; i < len; i++) {
+                temp2 = array[(i + 1) % len];
+                array[(i + 1) % len] = temp1;
+                temp1 = temp2;
+            }
+        }
     }
 
     static void cycleSwap(int[] array, int shift) {
+        int len = array.length;
+        if (len > 0) {
+            shift %= len;
 
+            for (int i = 0; i < shift; i++) {
+                cycleSwap(array);
+            }
+        }
     }
 }
--- src/test/java/com/epam/rd/autotasks/Main.java
+++ src/test/java/com/epam/rd/autotasks/Main.java
@@ -10,12 +10,12 @@ public class Main {
             System.out.println(Arrays.toString(array));
         }
         {
-            int[] array = new int[]{1, 3, 2, 7, 4};
-            CycleSwap.cycleSwap(array, 2);
+            int[] array = new int[]{1, 2, 3, 4, 5, 5, 6, 7, 8, 9};
+            CycleSwap.cycleSwap(array, 3);
             System.out.println(Arrays.toString(array));
         }
         {
-            int[] array = new int[]{1, 3, 2, 7, 4};
+            int[] array = new int[]{};
             CycleSwap.cycleSwap(array, 5);
             System.out.println(Arrays.toString(array));
         }
